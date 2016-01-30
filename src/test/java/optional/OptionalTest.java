bfea0ddab91dc014f82323388aabfdb1ae56ebe5
Haozhe Xu <jamesxuhaozhe@gmail.com>
2016-01-30 21:50:44 +0800

    add some test for Optional

---
+++ src/test/java/optional/OptionalTest.java
@@ -0,0 +1,18 @@
+package optional;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+
+/**
+ * Created by haozhexu on 1/30/16.
+ */
+public class OptionalTest {
+
+    @Test
+    public void testOf() throws Exception {
+        Assert.assertEquals(Integer.valueOf(1), Optional.of(1).get());
+        Assert.assertEquals(Integer.valueOf(1), Optional.of(1).orElse(2));
+    }
+}
