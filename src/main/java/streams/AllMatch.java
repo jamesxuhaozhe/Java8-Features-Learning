0ad104877e9dfed0d9181749216f0c9fcccf84db
Xu Haozhe <xuhaozhe@ibike668.com>
2018-10-17 09:02:48 +0800

    Modify AllMatch class to use static method

--- src/main/java/streams/AllMatch.java
+++ src/main/java/streams/AllMatch.java
@@ -6,41 +6,35 @@ import car.DealerShip;
 import java.util.List;
 
 /**
+ * A class that demonstrates allMatch functions
  * Created by haozhexu on 1/27/16.
  */
 public class AllMatch {
-    private List<Car> cars;
-
-    public AllMatch(List<Car> cars) {
-        this.cars = cars;
-    }
-
     /**
      * Return true if all the dealership cars are foreign made.
      */
-    public boolean areTheyAllForeignCars() {
+    public static boolean areTheyAllForeignCars(final List<Car> cars) {
         return cars.stream().allMatch(car -> car.isForeign());
     }
 
     /**
      * Return true if all the dealership cars are red.
      */
-    public boolean areTheyAllRed() {
+    public static boolean areTheyAllRed(List<Car> cars) {
         return cars.stream().allMatch(car -> car.getColor().equals("Red"));
     }
 
     /**
      * Return true if all the dealership cars are brand new.
      */
-    public boolean areTheyAllBrandNew() {
+    public static boolean areTheyAllBrandNew(List<Car> cars) {
         return cars.stream().allMatch(car -> car.IsBrandNew());
     }
 
     public static void main(final String[] args) {
         DealerShip dealerShip = new DealerShip();
-        AllMatch allMatch = new AllMatch(dealerShip.getInventory());
-        System.out.println(allMatch.areTheyAllForeignCars() + "\n");
-        System.out.println(allMatch.areTheyAllRed() + "\n");
-        System.out.println(allMatch.areTheyAllBrandNew() + "\n");
+        System.out.println(AllMatch.areTheyAllForeignCars(dealerShip.getInventory()) + "\n");
+        System.out.println(AllMatch.areTheyAllRed(dealerShip.getInventory()) + "\n");
+        System.out.println(AllMatch.areTheyAllBrandNew(dealerShip.getInventory()) + "\n");
     }
 }
